bf8a8ecbcbedff3da9f451baa92b21638e2d9123
Coty Condry <ccondry@cisco.com>
2020-10-26 23:01:03 -0500

    return this package version in environment info

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const pkg = require('../../package.json')
 
 // get demo environment info
 router.get('/', async (req, res, next) => {
   try {
     return res.status(200).send({
-      did: process.env.DID
+      did: process.env.DID,
+      version: pkg.version
     })
   } catch (e) {
     console.log(`Failed to get server info:`, e.message)
