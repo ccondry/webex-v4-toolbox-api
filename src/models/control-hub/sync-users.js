01a30184135d6b6d7575129329241643429ebfbb
Coty Condry <ccondry@cisco.com>
2021-02-10 15:12:06 -0600

    fix control hub user sync

--- src/models/control-hub/sync-users.js
+++ src/models/control-hub/sync-users.js
@@ -16,11 +16,9 @@ async function getToken () {
 // Sync CJP Users to Control Hub
 module.exports = async function () {
   try {
-    const url = 'https://ums.produs1.ciscoccservice.com/ums/users/synchronize'
+    const url = 'https://ums.produs1.ciscoccservice.com/ums/users/v2/synchronize/' + orgId
 
     const token = await getToken()
-
-    const body = {orgId}
 
     const options = {
       method: 'POST',
