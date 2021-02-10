b0ac9edfa8729dad455ef053c7c37bcdb807d4d2
Coty Condry <ccondry@cisco.com>
2021-02-10 14:14:58 -0600

    fix export syntax

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
 // parse JWT to JSON
-export const parseJwt = function (token) {
+function parseJwt (token) {
   const base64Url = token.split('.')[1]
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
   return JSON.parse(Buffer.from(base64, 'base64').toString())
+}
+
+module.exports = {
+  parseJwt
 }
