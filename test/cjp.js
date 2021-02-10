39084a1383f5077bb91c407a8b70edfff7701db6
Coty Condry <ccondry@cisco.com>
2021-02-10 15:12:47 -0600

    test cjp user list

--- test/cjp.js
+++ test/cjp.js
@@ -1,5 +1,15 @@
 require('dotenv').config()
 const cjp = require('../src/models/cjp')
 cjp.user.list()
-.then(allCjpUsers => console.log('allCjpUsers', allCjpUsers.details.users))
+.then(allCjpUsers => {
+  console.log('allCjpUsers', allCjpUsers.map(v => {
+    return {
+      id: v.id,
+      login: v.attributes.login__s,
+      skill: v.attributes.skillProfileId__s
+    }
+  }))
+  // const rick = allCjpUsers.find(v => v.attributes.login === 'rbarrows1234@cc1.dc-01.com')
+  // console.log('rick', rick)
+})
 .catch(e => console.log('error', e))
