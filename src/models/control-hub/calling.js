4024be8ea3f7c5fd1d9faf6ee27853893dbc316e
Coty Condry <ccondry@cisco.com>
2021-02-10 15:49:53 -0600

    add note for webex calling

---
+++ src/models/control-hub/calling.js
@@ -0,0 +1,5 @@
+
+// get webex calling phone numbers list for assigning webex calling extension
+// curl 'https://rialto.broadcloudpbx.com/enterpriseportalapi/v1.0/customers/325346/sites/325347/phoneassignments?filter=status%3Dunassigned&limit=200&offset=0&sort=phoneNumber' \
+// use control hub bearer token
+
