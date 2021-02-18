ac62c31c5f12d48cbbca1d5e241a99a908d2c9c2
Coty Condry <ccondry@cisco.com>
2021-02-18 00:26:18 -0600

    try/catch getProvisionDeletingUsers, and return empty array if license max is not reached

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -23,64 +23,71 @@ async function getProvisionStartedUsers () {
 }
 
 async function getProvisionDeletingUsers () {
-  // return array users who need to be deprovisioned 
-  // const query = {'demo.webex-v4prod.provision': 'deleting'}
-  // const projection = {
-  //   demo: false,
-  //   password: false
-  // }
-  // return db.find('toolbox', 'users', query, projection)
-
-  // wait for globals to exist
-  await Promise.resolve(globals.initialLoad)
-  // max number of users that can be provisioned. more than this will trigger
-  // deprovision. this is number of toolbox users.
-  const maxUsers = parseInt(globals.get('webexV4MaxUsers'))
-
-  // find license usage in control hub
-  const client = await ch.getClient()
-  const licenseUsage = await client.org.getLicenseUsage()
-  const cjpPremiumLicenses = licenseUsage[0].licenses.find(v => v.offerName === 'CJPPRM')
-  // if license usage of CJP premium is > 95%
-  // if (cjpPremiumLicenses.usage / cjpPremiumLicenses.volume >= 0.95) {
-  // if (cjpPremiumLicenses.volume - cjpPremiumLicenses.usage <= 10) {
-  // console.log('cjpPremiumLicenses.usage', cjpPremiumLicenses.usage)
-  // console.log('maxUsers', maxUsers)
-  if (cjpPremiumLicenses.usage > maxUsers) {
-    // too full - need to deprovision some users
-    // get all control hub users
-    const allUsers = await client.user.listAll()
-    // filter control hub users that do not have CJPPRM license
-    const licensedUsers = allUsers.filter(user => {
-      try {
-        const regex = /\d{4}/
-        // true if username contains 4-digit ID
-        return user.userName.slice(8, 12).match(regex) &&
-        user.licenseID.includes('CJPPRM_1cf76371-2fde-4f72-8122-b6a9d2f89c73')
-      } catch (e) {
-        return false
-      }
-    })
-    // find user provision info for this demo
-    const query = {'demo.webex-v4prod.lastAccess': {$exists: 1}, 'demo.webex-v4prod.provision': 'complete'}
-    // const projection = {password: false}
-    const projection = {id: true, 'demo.webex-v4prod.lastAccess': true}
-    const provisionedUsers = await db.find('toolbox', 'users', query, projection)
-    // console.log('provisionedUsers', provisionedUsers)
-    // filter provisioned toolbox users to those with matching licensed control hub users
-    const userMap = provisionedUsers.filter(user => {
-      return licensedUsers.find(agent => user.id === agent.userName.slice(8, 12))
-    })
-    // sort by last access time descending
-    userMap.sort((a, b) => {
-      const aDate = new Date(a.demo['webex-v4prod'].lastAccess || 0)
-      const bDate = new Date(b.demo['webex-v4prod'].lastAccess || 0)
-      // descending
-      return bDate - aDate
-    })
-    // console.log('userMap', userMap)
-    // keep top users, return the rest
-    return userMap.slice(maxUsers)
+  try {
+    // return array users who need to be deprovisioned 
+    // const query = {'demo.webex-v4prod.provision': 'deleting'}
+    // const projection = {
+    //   demo: false,
+    //   password: false
+    // }
+    // return db.find('toolbox', 'users', query, projection)
+  
+    // wait for globals to exist
+    await Promise.resolve(globals.initialLoad)
+    // max number of users that can be provisioned. more than this will trigger
+    // deprovision. this is number of toolbox users.
+    const maxUsers = parseInt(globals.get('webexV4MaxUsers'))
+  
+    // find license usage in control hub
+    const client = await ch.getClient()
+    const licenseUsage = await client.org.getLicenseUsage()
+    const cjpPremiumLicenses = licenseUsage[0].licenses.find(v => v.offerName === 'CJPPRM')
+    // if license usage of CJP premium is > 95%
+    // if (cjpPremiumLicenses.usage / cjpPremiumLicenses.volume >= 0.95) {
+    // if (cjpPremiumLicenses.volume - cjpPremiumLicenses.usage <= 10) {
+    // console.log('cjpPremiumLicenses.usage', cjpPremiumLicenses.usage)
+    // console.log('maxUsers', maxUsers)
+    if (cjpPremiumLicenses.usage > maxUsers) {
+      // too full - need to deprovision some users
+      // get all control hub users
+      const allUsers = await client.user.listAll()
+      // filter control hub users that do not have CJPPRM license
+      const licensedUsers = allUsers.filter(user => {
+        try {
+          const regex = /\d{4}/
+          // true if username contains 4-digit ID
+          return user.userName.slice(8, 12).match(regex) &&
+          user.licenseID.includes('CJPPRM_1cf76371-2fde-4f72-8122-b6a9d2f89c73')
+        } catch (e) {
+          return false
+        }
+      })
+      // find user provision info for this demo
+      const query = {'demo.webex-v4prod.lastAccess': {$exists: 1}, 'demo.webex-v4prod.provision': 'complete'}
+      // const projection = {password: false}
+      const projection = {id: true, 'demo.webex-v4prod.lastAccess': true}
+      const provisionedUsers = await db.find('toolbox', 'users', query, projection)
+      // console.log('provisionedUsers', provisionedUsers)
+      // filter provisioned toolbox users to those with matching licensed control hub users
+      const userMap = provisionedUsers.filter(user => {
+        return licensedUsers.find(agent => user.id === agent.userName.slice(8, 12))
+      })
+      // sort by last access time descending
+      userMap.sort((a, b) => {
+        const aDate = new Date(a.demo['webex-v4prod'].lastAccess || 0)
+        const bDate = new Date(b.demo['webex-v4prod'].lastAccess || 0)
+        // descending
+        return bDate - aDate
+      })
+      // console.log('userMap', userMap)
+      // keep top users, return the rest
+      return userMap.slice(maxUsers)
+    } else {
+      // not full - return empty array
+      return []
+    }
+  } catch (e) {
+    throw e
   }
 }
 
