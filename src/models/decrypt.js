f91dafda09601280c273e484d5633142db10693f
Coty Condry <ccondry@cisco.com>
2021-02-24 21:30:39 -0600

    reset padding type

--- src/models/decrypt.js
+++ src/models/decrypt.js
@@ -15,7 +15,7 @@ module.exports = function (encryptedData) {
       // In order to decrypt the data, we need to specify the
       // same hashing function and padding scheme that we used to
       // encrypt the data in the previous step
-      padding: crypto.constants.RSA_PKCS1_PADDING,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
       oaepHash: 'sha256'
     },
     buf
