8347aaeb43e787947a08250bd9ef775046fa391d
Coty Condry <ccondry@cisco.com>
2021-02-24 20:21:48 -0600

    temporary debug log for decyption errors

--- src/models/decrypt.js
+++ src/models/decrypt.js
@@ -1,11 +1,14 @@
 const crypto = require('crypto')
 const fs = require('fs')
-const path = require('path')
+// const path = require('path')
 
 // my private cert
 const key = fs.readFileSync('./certs/rsa-private.pem')
 
 module.exports = function (encryptedData) {
+  console.log('encryptedData', encryptedData)
+  const buf = Buffer.from(encryptedData, 'base64')
+  console.log('buf', buf)
   const decrypted = crypto.privateDecrypt(
     {
       key,
@@ -15,7 +18,10 @@ module.exports = function (encryptedData) {
       padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
       oaepHash: 'sha256'
     },
-    Buffer.from(encryptedData, 'base64')
+    buf
   )
-  return decrypted.toString('utf8')
+  console.log('decrypted', decrypted)
+  const utf8 = decrypted.toString('utf8')
+  console.log('utf8', utf8)
+  return utf8
 }
