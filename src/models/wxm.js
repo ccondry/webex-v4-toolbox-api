6be1e714b3240224af19e8e1231ca86a8661e159
Coty Condry <ccondry@cisco.com>
2021-05-06 09:25:54 -0500

    try using existing WXM credentials to POST the WXM mapUsers request

--- src/models/wxm.js
+++ src/models/wxm.js
@@ -1,12 +1,15 @@
 const fetch = require('./fetch')
+const globals = require('./globals')
 
 // map control hub user ID to username in WXM. enables users for the WXM gadget.
 // users is an array of control hub user objects
 async function mapUsers (orgId, users) {
   const url = 'https://api.getcloudcherry.com/api/account/UpdateExternalIdp/Bulk'
-
   const options = {
     method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + globals.get('wxmCreds')
+    },
     body: {
       ExternalIdentityProviderName: 'webexci',
       IDPMapppings: users.map(user => {
