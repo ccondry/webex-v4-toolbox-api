86e25407450f2a5492ac394464f5a53ca30e8f79
Coty Condry <ccondry@cisco.com>
2021-10-29 07:12:16 -0500

    fix random hour not including hour 23

--- src/models/provision.js
+++ src/models/provision.js
@@ -207,7 +207,7 @@ module.exports = async function (user) {
     // generate a random hour of the day for script start. this fixes
     // CCBU db query/logging issue when many current routing strategies are
     // generated at the same time
-    const randomHour = Math.floor(Math.random() * 23)
+    const randomHour = Math.floor(Math.random() * 24)
     const randomTime = randomHour * 60 * 60 * 1000
     
     // chat entry point routing strategy
--- test/random-time.js
+++ test/random-time.js
@@ -1,6 +1,6 @@
 // generate a random hour of the day for script start. this fixes
 // CCBU db query/logging issue when many current routing strategies are
 // generated at the same time
-const randomHour = Math.floor(Math.random() * 23)
+const randomHour = Math.floor(Math.random() * 24)
 const randomTime = randomHour * 60 * 60 * 1000
-console.log(String(randomTime).padEnd(8, '0'))
+console.log(String(randomTime))
